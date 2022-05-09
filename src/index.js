f29582d301d3c80edea40a75cde883cfe7ee5cc7
Violetta <viol.fedoseenko@yandex.ru>
2022-05-09 22:30:16 +0300

    add:displaying letters in textarea and  button illumination

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ keyboardWrapper.appendChild(keyboardLeyout);
 
 keyboardWrapper.classList.add('keyboard_wrapper');
 keyboardLeyout.classList.add('keyboard_leyout');
+textarea.classList.add('textarea');
 
 title.textContent = 'RSS Виртуальная клавиатура';
 subtitle.textContent = 'Клавиатура создана в операционной системе Windows\nДля переключения языка комбинация: Ctrl + Alt';
@@ -30,13 +31,18 @@ class Keyboard {
   }
 
   generateButtons() {
+    this.keyboardLeyout.classList.add('keyboard');
     keyButtons.forEach((key) => {
       const button = document.createElement('button');
       button.setAttribute('id', key.code);
       button.classList.add('my_button', key.code);
-
+      // button.setAttribute('type', 'button');
       button.setAttribute('content_en', key.content.en);
       button.setAttribute('content_ru', key.content.ru);
+      button.setAttribute('shift_en', key.shift.en);
+      button.setAttribute('shift_ru', key.shift.ru);
+      button.setAttribute('character_en', key.character.en);
+      button.setAttribute('character_ru', key.character.ru);
 
       if (this.language === 'en') {
         button.textContent = key.content.en;
@@ -48,9 +54,78 @@ class Keyboard {
       this.keys.push(button);
     });
   }
-}
 
+
+  addKeyboardListeners() {
+    const cursorStart = this.textarea.selectionStart;
+    const cursorEnd = this.textarea.selectionEnd;
+    document.addEventListener('keydown', (keyEvent) => {
+      const key = document.getElementById(keyEvent.code);
+      if (key) {
+        key.classList.add('active');
+        keyEvent.preventDefault();
+        console.log(key);
+        if (key.getAttribute('character_en') !== 'func' || key.getAttribute('character_ru') !== 'func') {
+          this.textarea.value += key.innerText;
+        }
+        if (key.getAttribute('content_en') === 'Backspace') {
+          let str = this.textarea.value;
+          if (cursorStart === cursorEnd) {
+            str = str.substr(0, (str.length - 1));
+            this.textarea.value = str;
+          }
+        }
+      }
+      if (key.getAttribute('content_en') === 'Enter') {
+        let str = this.textarea.value;
+        str += '\n';
+        this.textarea.value = str;
+      }
+      if (key.getAttribute('content_en') === 'Tab') {
+        let str = this.textarea.value;
+        str += '    ';
+        this.textarea.value = str;
+      }
+      if (key.getAttribute('content_en') === ' ') {
+        let str = this.textarea.value;
+        str += ' ';
+        this.textarea.value = str;
+      }
+      if (key.getAttribute('content_en') === 'Ctrl' && key.getAttribute('content_en') === 'Alt') {
+        this.language = this.language === 'ru' ? 'en' : 'ru';
+        localStorage.setItem('language', this.language);
+        console.log(localStorage);
+      }
+    });
+    
+    document.addEventListener('keyup', (keyButton) => {
+      const key = document.getElementById(keyButton.code);
+      if (key) {
+        if (keyButton.code !== 'CapsLock') key.classList.remove('active');
+        if (keyButton.code === 'ShiftLeft' || keyButton.code === 'ShiftRight') this.shiftText(true);
+      }
+    });
+
+    this.keyboardLeyout.addEventListener('mousedown', (event) => {
+      const eventKeyDown = new KeyboardEvent('keydown', {
+        code: event.target.id,
+      });
+      document.dispatchEvent(eventKeyDown);
+      this.isDown = true;
+      console.log(event.target.id);
+    });
+
+    this.keyboardLeyout.addEventListener('mouseup', (event) => {
+      const eventKeyUp = new KeyboardEvent('keyup', { code: event.target.id });
+      document.dispatchEvent(eventKeyUp);
+      this.isDown = false;
+    });
+  }
+}
 const keyboardObj = new Keyboard(keyboardLeyout, textarea);
 keyboardObj.generateButtons();
 
+keyboardObj.addKeyboardListeners();
 
+textarea.onblur = () => textarea.focus();
+textarea.focus();
